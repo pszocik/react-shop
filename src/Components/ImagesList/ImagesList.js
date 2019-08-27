0123ccb5ab656b7beedd5913b52cefa1cfb080e5
mabbyn <Piotrek.szocik@gmail.com>
2019-08-27 15:45:58 +0200

    added buttons

--- src/Components/ImagesList/ImagesList.js
+++ src/Components/ImagesList/ImagesList.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./ImagesList.css";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
 export default function ImagesList({ Images, loading }) {
   if (loading) {
@@ -8,7 +10,17 @@ export default function ImagesList({ Images, loading }) {
   return (
     <div className="ImagesList">
       {Images.map(el => {
-        return <img key={el.id} alt={el.id} src={el.link} />;
+        return (
+          <div className="image">
+            <img key={el.id} alt={el.id} src={el.link} />
+            <button className="image-button">
+              <MonetizationOnIcon />
+            </button>
+            <button className="image-button">
+              <AddShoppingCartIcon />
+            </button>
+          </div>
+        );
       })}
     </div>
   );
