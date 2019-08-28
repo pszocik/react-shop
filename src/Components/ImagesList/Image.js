3646747718e79c9d51d095b212edc674e6053e1b
mabbyn <Piotrek.szocik@gmail.com>
2019-08-28 15:57:46 +0200

    style changed

--- src/Components/ImagesList/Image.js
+++ src/Components/ImagesList/Image.js
@@ -6,10 +6,16 @@ const Image = ({ id, link, paymentPopUp }) => {
   return (
     <div key={id} className="image">
       <img key={id} alt={id} src={link} />
-      <button onClick={() => paymentPopUp(id)} id={id} className="image-button">
+      <button
+        onClick={() => paymentPopUp(id)}
+        id={id}
+        className="image-button image-payment"
+      >
+        <span className="image-text image-text-payment">Purchase</span>
         <MonetizationOnIcon />
       </button>
-      <button className="image-button">
+      <button className="image-button image-cart ">
+        <span className="image-text image-text-cart">Add to cart</span>
         <AddShoppingCartIcon />
       </button>
     </div>
