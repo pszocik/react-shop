20b76cb2b493104f381ed2a8c05dc4db88b15d4a
mabbyn <Piotrek.szocik@gmail.com>
2019-08-28 17:37:02 +0200

    button styles (still need some changes)

--- src/Components/ImagesList/Image.js
+++ src/Components/ImagesList/Image.js
@@ -6,18 +6,20 @@ const Image = ({ id, link, paymentPopUp }) => {
   return (
     <div key={id} className="image">
       <img key={id} alt={id} src={link} />
-      <button
-        onClick={() => paymentPopUp(id)}
-        id={id}
-        className="image-button image-payment"
-      >
-        <span className="image-text image-text-payment">Purchase</span>
-        <MonetizationOnIcon />
-      </button>
-      <button className="image-button image-cart ">
-        <span className="image-text image-text-cart">Add to cart</span>
-        <AddShoppingCartIcon />
-      </button>
+      <div className="image-button image-payment">
+        <button
+          className="image-button-payment"
+          onClick={() => paymentPopUp(id)}
+          id={id}
+        >
+          <MonetizationOnIcon />
+        </button>
+      </div>
+      <div className="image-button image-cart">
+        <button className="image-button-cart">
+          <AddShoppingCartIcon />
+        </button>
+      </div>
     </div>
   );
 };
